bdb06e1e5d2e3f2c67796a06ca9f5834a3165baf
justinamoran <jmoran1@mail.austral.edu.ar>
2025-04-18 18:02:20 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,9 @@
 def leap_year():
-    print("TO DO")
+    year = int(input("Ingrese un año:"))
+    type1 = (year%100==0 and year%400==0)
+    type2 = (year%100>0 and year%4==0)
+    type3 = (year%100>0 and year%4>0)
+    if type1==True or type2==True:
+        print(f"El año {year} es bisiesto")
+    else:
+        print(f"El año {year} no es bisiesto")
